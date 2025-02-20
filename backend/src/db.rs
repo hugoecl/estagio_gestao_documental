c6a22c1e68d14cfe298a451ffef2846fc1712419
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-02-20 09:17:45 +0000

    Start making the db schema

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,5 +1,9 @@
 use sqlx::mysql::MySqlPool;
 
+use crate::utils::hashing_utils::hash;
+
+const SCHEMA: &str = include_str!("../sql/schema.sql");
+
 pub struct Db {
     pub pool: MySqlPool,
 }
@@ -7,6 +11,23 @@ pub struct Db {
 impl Db {
     pub async fn new() -> Result<Db, sqlx::Error> {
         let pool = MySqlPool::connect("mysql://root:root@localhost:3306/gestao_documental").await?;
+
+        sqlx::query(SCHEMA).execute(&pool).await?;
+
+        let admin_password = hash("admin");
+
+        sqlx::query!(
+            "INSERT IGNORE INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?)",
+            "admin",
+            "admin@gmail.com",
+            &admin_password[..],
+            true
+        )
+        .execute(&pool)
+        .await?;
+
+        println!("Connected to Database");
+
         Ok(Db { pool })
     }
 }
