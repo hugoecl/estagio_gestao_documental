7a394305d1c299f30cd629280f173f5220d42031
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-02-21 17:29:51 +0000

    Start implementing collapse component

---
+++ frontend/src/components/Collapse.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  const { title, children }: { title: string; children: Snippet } = $props();
+
+  let isOpen = $state(false);
+</script>
+
+<div class="collapse collapse-arrow bg-base-100 border border-base-300">
+  <input
+    type="radio"
+    name={title}
+    onmousedown={(e) => {
+      e.preventDefault();
+      e.currentTarget.checked = !e.currentTarget.checked;
+    }}
+  />
+  <div class="collapse-title font-semibold">{title}</div>
+
+  <div class="collapse-content text-sm">
+    {@render children?.()}
+  </div>
+</div>
+
+<style>
+  .collapse {
+    border-radius: 0.3rem;
+  }
+  input {
+    cursor: pointer;
+  }
+</style>
