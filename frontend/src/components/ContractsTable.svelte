ece1450b67845d7a3458418f3a8519311cccbefc
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-03-05 09:21:10 +0000

    Start making the table

---
+++ frontend/src/components/ContractsTable.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+</script>
+
+<div
+  class="overflow-x-auto rounded-box border border-base-content/5 bg-base-200"
+>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Fornecedor</th>
+        <th>Número de Contrato</th>
+        <th>Data</th>
+        <th>Data Início</th>
+        <th>Data Fim</th>
+        <th>Tipo</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="hover:bg-base-300">
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>123123</td>
+        <td>12/01/2025</td>
+        <td>06/02/2025</td>
+        <td>06/04/2025</td>
+        <td>Novo</td>
+        <td>Ativo</td>
+      </tr>
+      <tr class="hover:bg-base-300">
+        <th>2</th>
+        <td>Cy Ganderton</td>
+        <td>123123</td>
+        <td>12/01/2025</td>
+        <td>06/02/2025</td>
+        <td>06/04/2025</td>
+        <td>Novo</td>
+        <td>Ativo</td>
+      </tr>
+      <tr class="hover:bg-base-300">
+        <th>3</th>
+        <td>Cy Ganderton</td>
+        <td>123123</td>
+        <td>12/01/2025</td>
+        <td>06/02/2025</td>
+        <td>06/04/2025</td>
+        <td>Novo</td>
+        <td>Ativo</td>
+      </tr>
+      <tr class="hover:bg-base-300">
+        <th>4</th>
+        <td>Cy Ganderton</td>
+        <td>123123</td>
+        <td>12/01/2025</td>
+        <td>06/02/2025</td>
+        <td>06/04/2025</td>
+        <td>Novo</td>
+        <td>Ativo</td>
+      </tr>
+    </tbody>
+  </table>
+  <div class="flex justify-between items-center p-2 bg-base-100">
+    <span>A mostrar 1 a 10 de 97 resultados</span>
+    <div class="join">
+      <button class="join-item btn">1</button>
+      <button class="join-item btn">2</button>
+      <button class="join-item btn btn-disabled">...</button>
+      <button class="join-item btn">99</button>
+      <button class="join-item btn">100</button>
+    </div>
+  </div>
+</div>
