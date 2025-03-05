ca3af69ceac8c069492b3b3ce2fbe3ed3857654e
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-03-05 11:47:14 +0000

    only renew every 5 minutes instead of renew per request

--- backend/src/utils/session_utils.rs
+++ backend/src/utils/session_utils.rs
@@ -4,11 +4,20 @@ use actix_web::HttpResponse;
 pub fn validate_session(session: &Session) -> Result<i32, HttpResponse> {
     let user_id = session.get::<i32>("user_id").unwrap_or(None);
 
-    match user_id {
-        Some(id) => {
+    if let Some(last_renewal) = session.get::<i64>("last_renewal").unwrap_or(None) {
+        let now = chrono::Utc::now().timestamp();
+        if now - last_renewal > 300 {
             session.renew();
-            Ok(id)
+            session.insert("last_renewal", now).unwrap();
         }
+    } else {
+        session
+            .insert("last_renewal", chrono::Utc::now().timestamp())
+            .unwrap();
+    }
+
+    match user_id {
+        Some(id) => Ok(id),
         None => Err(HttpResponse::Unauthorized().finish()),
     }
 }
