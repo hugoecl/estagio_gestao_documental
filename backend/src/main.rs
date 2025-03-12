f04c7cab44e288ec69432176ffd998182cbe4347
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-03-12 17:05:17 +0000

    Add https to the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::BufReader};
+
 use actix_cors::Cors;
 use actix_session::{SessionMiddleware, config::PersistentSession, storage::CookieSessionStore};
 use actix_web::{
@@ -20,21 +22,33 @@ mod routes;
 mod utils;
 
 use db::{Cache, Db};
+use rustls::{ServerConfig, pki_types::PrivateKeyDer};
+use rustls_pemfile::{certs, pkcs8_private_keys};
 
 struct State {
     db: Db,
     cache: Cache,
 }
 
+#[derive(argh::FromArgs)]
+/// Start the server
+struct CliArgs {
+    /// the port to run the server on
+    #[argh(option, short = 'p', default = "1234")]
+    port: u16,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args: CliArgs = argh::from_env();
+    let port = args.port;
     let addrs: &str;
     if cfg!(debug_assertions) {
         addrs = "localhost";
-        println!("Development Server running at http://{}:1234", addrs);
+        println!("Development Server running at http://{}:{}", addrs, port);
     } else {
         addrs = "0.0.0.0";
-        println!("Production Server running at https://{}:1234", addrs);
+        println!("Production Server running at https://{}:{}", addrs, port);
     }
 
     let key = Key::generate();
@@ -49,80 +63,80 @@ async fn main() -> std::io::Result<()> {
 
     let state = web::Data::new(State { db, cache });
 
-    #[cfg(feature = "log")]
     const LOG_LEVEL: &str = if cfg!(debug_assertions) {
         "debug"
     } else {
         "info"
     };
-
-    #[cfg(feature = "log")]
     env_logger::init_from_env(env_logger::Env::new().default_filter_or(LOG_LEVEL));
+
     // TODO: See the cookie warning
-
-    if cfg!(feature = "log") {
+    if cfg!(debug_assertions) {
         HttpServer::new(move || {
-            let session_middleware = if cfg!(debug_assertions) {
-                SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
-                    .cookie_secure(false)
-                    .cookie_http_only(false)
-                    .cookie_same_site(actix_web::cookie::SameSite::Strict)
-                    .session_lifecycle(
-                        PersistentSession::default().session_ttl(Duration::seconds(SECS_IN_WEEK)),
-                    )
-                    .build()
-            } else {
-                SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
-                    .cookie_secure(true)
-                    .cookie_http_only(true)
-                    .cookie_same_site(actix_web::cookie::SameSite::None)
-                    .session_lifecycle(
-                        PersistentSession::default().session_ttl(Duration::seconds(SECS_IN_WEEK)),
-                    )
-                    .build()
-            };
-
             App::new()
                 .configure(routes::init)
                 .wrap(Cors::permissive())
-                .wrap(session_middleware)
+                .wrap(
+                    SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
+                        .cookie_secure(false)
+                        .cookie_http_only(false)
+                        .cookie_same_site(actix_web::cookie::SameSite::Strict)
+                        .session_lifecycle(
+                            PersistentSession::default()
+                                .session_ttl(Duration::seconds(SECS_IN_WEEK)),
+                        )
+                        .build(),
+                )
                 .wrap(actix_web::middleware::Compress::default())
                 .wrap(actix_web::middleware::Logger::default())
                 .app_data(state.clone())
         })
-        .bind((addrs, 1234))?
+        .bind((addrs, port))?
         .run()
         .await
     } else {
-        HttpServer::new(move || {
-            let session_middleware = if cfg!(debug_assertions) {
-                SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
-                    .cookie_secure(false)
-                    .cookie_http_only(false)
-                    .cookie_same_site(actix_web::cookie::SameSite::Strict)
-                    .session_lifecycle(
-                        PersistentSession::default().session_ttl(Duration::seconds(SECS_IN_WEEK)),
-                    )
-                    .build()
-            } else {
-                SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
-                    .cookie_secure(true)
-                    .cookie_http_only(true)
-                    .cookie_same_site(actix_web::cookie::SameSite::None)
-                    .session_lifecycle(
-                        PersistentSession::default().session_ttl(Duration::seconds(SECS_IN_WEEK)),
-                    )
-                    .build()
-            };
+        rustls::crypto::aws_lc_rs::default_provider()
+            .install_default()
+            .unwrap();
 
+        let config = ServerConfig::builder().with_no_client_auth();
+
+        let cert_file = &mut BufReader::new(File::open("certs/cert.pem").unwrap());
+        let key_file = &mut BufReader::new(File::open("certs/key.pem").unwrap());
+
+        let cert_chain = certs(cert_file).collect::<Result<Vec<_>, _>>().unwrap();
+        let mut keys = pkcs8_private_keys(key_file)
+            .map(|key| key.map(PrivateKeyDer::Pkcs8))
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap();
+
+        if keys.is_empty() {
+            eprintln!("Could not locate PKCS 8 private keys.");
+            std::process::exit(1);
+        }
+
+        let config = config.with_single_cert(cert_chain, keys.remove(0)).unwrap();
+
+        HttpServer::new(move || {
             App::new()
                 .configure(routes::init)
                 .wrap(Cors::permissive())
-                .wrap(session_middleware)
+                .wrap(
+                    SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
+                        .cookie_secure(true)
+                        .cookie_http_only(true)
+                        .cookie_same_site(actix_web::cookie::SameSite::None)
+                        .session_lifecycle(
+                            PersistentSession::default()
+                                .session_ttl(Duration::seconds(SECS_IN_WEEK)),
+                        )
+                        .build(),
+                )
                 .wrap(actix_web::middleware::Compress::default())
+                .wrap(actix_web::middleware::Logger::default())
                 .app_data(state.clone())
         })
-        .bind((addrs, 1234))?
+        .bind_rustls_0_23(&format!("0.0.0.0:{}", port), config)?
         .run()
         .await
     }
--- frontend/src/api/base-url.ts
+++ frontend/src/api/base-url.ts
@@ -1,1 +1,1 @@
-export default "http://localhost:1234";
+export default "https://127.0.0.1:1234";
