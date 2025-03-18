926fd231adfa33c8861710e971292a4bc4eb0c09
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-03-18 10:52:36 +0000

    change work_contracts to work-contracts

--- backend/src/routes/work_contract_routes.rs
+++ backend/src/routes/work_contract_routes.rs
@@ -2,7 +2,7 @@ use actix_web::web;
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/work_contracts")
+        web::scope("/work-contracts")
             .route(
                 "",
                 web::get().to(crate::handlers::work_contract_handlers::get_work_contracts),
