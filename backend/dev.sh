744c8ab0b55bfc1807347325d65d081ba44e71a3
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-03-26 15:53:16 +0000

    Improve compilation times even more by disabling build info from getting linked

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-RUSTFLAGS="-Z threads=4 -C link-arg=-fuse-ld=/usr/bin/mold" CARGO_PROFILE_DEV_CODEGEN_BACKEND=cranelift cargo build -Zcodegen-backend
+RUSTFLAGS="-Z threads=4 -C link-arg=-fuse-ld=/usr/bin/mold" CARGO_PROFILE_DEV_CODEGEN_BACKEND=cranelift cargo run -Zcodegen-backend
