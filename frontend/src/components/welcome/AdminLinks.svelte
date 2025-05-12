b31c8a69036d1e60ef1b0517cc53c60eb89363b2
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-05-12 11:06:19 +0100

    add link to admin in admin card

--- frontend/src/components/welcome/AdminLinks.svelte
+++ frontend/src/components/welcome/AdminLinks.svelte
@@ -4,17 +4,21 @@
         { title: "Gerir Páginas", href: "/admin/pages/", icon: "file-lines" },
         { title: "Gerir Utilizadores", href: "/admin/users/", icon: "users" },
         { title: "Gerir Funções", href: "/admin/roles/", icon: "user-tag" },
-        { title: "Enviar Notificações", href: "/admin/notifications/broadcast/", icon: "bullhorn" },
+        {
+            title: "Enviar Notificações",
+            href: "/admin/notifications/broadcast/",
+            icon: "bullhorn",
+        },
         // Add more admin links here if needed
     ];
 </script>
 
 <div class="card bg-base-100 shadow-xl">
     <div class="card-body">
-        <h2 class="card-title text-primary">
+        <a href="/admin/" class="card-title text-primary">
             <i class="fa-solid fa-user-shield mr-2"></i>
             Painel de Administração
-        </h2>
+        </a>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 mt-2">
             {#each adminLinks as link}
