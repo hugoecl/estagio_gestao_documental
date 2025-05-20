29407cc10cc30ddc5f005a9497395bac1eeb7d07
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-05-20 14:08:11 +0100

    delete duplicated migration

--- backend/migrations/20240630_add_can_add_permission.sql
+++
@@ -1,9 +0,0 @@
--- Migration to add can_add permission column to page_permissions
--- This permission allows users to add information to empty fields without editing existing data
-
--- Add the new column to page_permissions table
-ALTER TABLE page_permissions ADD COLUMN can_add BOOLEAN NOT NULL DEFAULT FALSE;
-
--- Set the permission to true for all roles that already have can_edit permission
--- This ensures backward compatibility
-UPDATE page_permissions SET can_add = can_edit WHERE 1=1; 
